358e861b613cf509c6c57361d06648da0a915122
Shawn Hill <shawn.hill121@gmail.com>
2018-02-13 18:50:54 -0500

    FizzBuzz project in C# v1.0

---
+++ fizzBuzz/fizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzzApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int count = 1; count <= 100; count++)
+            {
+                if (count % 3 == 0 && count % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz!!!");
+                }
+                else if (count % 3 == 0)
+                {
+                    Console.WriteLine("Fizz!");
+                }
+                else if (count % 5 == 0)
+                {
+                    Console.WriteLine("Buzz!");
+                }
+                else
+                {
+                    Console.WriteLine($"{count}");
+                }
+            }
+        }
+    }
+}
