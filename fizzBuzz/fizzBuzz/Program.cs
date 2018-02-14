01bd2716ce89a16ddd8447d6e71bc633767e5e59
Shawn Hill <shawn.hill121@gmail.com>
2018-02-13 21:17:09 -0500

    updates program.cs to run fizzbuzz from class

--- fizzBuzz/fizzBuzz/Program.cs
+++ fizzBuzz/fizzBuzz/Program.cs
@@ -6,25 +6,9 @@ namespace FizzBuzzApplication
     {
         static void Main(string[] args)
         {
-            for (int count = 1; count <= 100; count++)
-            {
-                if (count % 3 == 0 && count % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz!!!");
-                }
-                else if (count % 3 == 0)
-                {
-                    Console.WriteLine("Fizz!");
-                }
-                else if (count % 5 == 0)
-                {
-                    Console.WriteLine("Buzz!");
-                }
-                else
-                {
-                    Console.WriteLine($"{count}");
-                }
-            }
+            FizzBuzz startFizzing = new FizzBuzz();
+
+            startFizzing.RunFizzBuzz();
         }
     }
 }
