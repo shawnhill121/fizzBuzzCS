c0add883ef88669227a079ea044acc49579aee86
Shawn Hill <shawn.hill121@gmail.com>
2018-02-13 21:16:02 -0500

    creates fizzbuzz class

---
+++ fizzBuzz/fizzBuzz/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzApplication
+{
+    class FizzBuzz
+    {
+        public void RunFizzBuzz()
+        {
+            for (int count = 1; count <= 100; count++)
+            {
+                if (count % 3 == 0 && count % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz!!!");
+                }
+                else if (count % 3 == 0)
+                {
+                    Console.WriteLine("Fizz!");
+                }
+                else if (count % 5 == 0)
+                {
+                    Console.WriteLine("Buzz!");
+                }
+                else
+                {
+                    Console.WriteLine($"{count}");
+                }
+            }
+        }
+    }
+}
